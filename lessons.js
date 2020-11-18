42eb4fdc7a8ce42bc00ce2862a5044fb49a5f9ad
Akram Lagrazna <lagrazna@outlook.it>
2020-11-18 08:24:56 -0800

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -1,64 +1,87 @@
-var webstore = new Vue({ el: '#app', data: 
+var webstore = new Vue({ el: '#less', data: 
 {
-    sitename: "After School Club",
-    order: {
-        name: '',
-        phoneNumber: +44
+    "products": [        
+    {
+        id: 1001,     
+        subject: "Math",
+        location: "London",
+        price: 100,
+        image: "https://i.imgur.com/K4vuaWq.png",
+        spaces: 5
     },
-    product: 
-        {
-            id: 1001,     
-            subject: "Math",
-            location: "London",
-            price: 100,
-            image: "https://i.imgur.com/K4vuaWq.png",
-            spaces: 5
-},
-    cart : []
-},
-    methods: {
-        addToCart: function() {
-            if (this.product.spaces == 0) {
-                return;
-            } else {
-            this.product.spaces = this.product.spaces - 1;
-            this.cart.push(this.product.id);
-        }},
-        submitOrder: function() {
-            alert('Order Submitted!');
-        },        
-        showCheckout: function(){
-            this.showProduct = this.showProduct ? false : true;
-        }
+    {
+        id: 1002,     
+        subject: "English",
+        location: "London",
+        price: 80,
+        image: "https://i.imgur.com/k9c4XEd.jpeg",
+        spaces: 5
     },
-    computed: { 
-        cartItemCount: function() { 
-        return this.cart.length;
-        },
-        canShowAdd: function(){
-            if (this.product.spaces == 0) {
-                return false;
-            } else {
-                return true;
-            }
-        },
-        canShowCart: function(){
-            if (this.cartItemCount > 0) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        canCheckout: function(){
-            if (this.order.name != '' && this.order.phoneNumber != 44){
-                return true;
-        } else {
-            return false;
-        }},
-
+    {
+        id: 1003,     
+        subject: "Physics",
+        location: "London",
+        price: 90,
+        image: "https://digitaldefynd.com/wp-content/uploads/2019/03/best-Physics-course-class-certification-training-online.jpg",
+        spaces: 5
+    },
+    {
+        id: 1004,     
+        subject: "Computer Science",
+        location: "London",
+        price: 55,
+        image: "https://static.techspot.com/images2/news/bigimage/2019/01/2019-01-25-image-3.jpg",
+        spaces: 5
+    },
+    {
+        id: 1005,     
+        subject: "Film",
+        location: "London",
+        price: 65,
+        image: "https://sites.google.com/a/crschools.org/home/_/rsrc/1544480884136/middle/middle-school-teacher-webpages/mrs-denisons-language-arts-classroom/film-and-writing-overview/clipartbest-com-2JDmK0-clipart.jpeg?height=222&width=400",
+        spaces: 5
+    },
+    {
+        id: 1006,     
+        subject: "History",
+        location: "London",
+        price: 45,
+        image: "https://image.shutterstock.com/image-photo/person-writing-blackboard-during-history-260nw-723428044.jpg",
+        spaces: 5
+    },
+    {
+        id: 1007,     
+        subject: "Italian",
+        location: "London",
+        price: 110,
+        image: "https://upload.wikimedia.org/wikipedia/en/0/03/Flag_of_Italy.svg",
+        spaces: 5
+    },
+    {
+        id: 1008,     
+        subject: "French",
+        location: "London",
+        price: 75,
+        image: "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg",
+        spaces: 5
+    },
+    {
+        id: 1009,     
+        subject: "German",
+        location: "London",
+        price: 85,
+        image: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/220px-Flag_of_Germany.svg.png",
+        spaces: 5
+    },
+    {
+        id: 1010,     
+        subject: "Geography",
+        location: "London",
+        price: 95,
+        image: "https://media.salon.com/2015/08/globe_books.jpg",
+        spaces: 5
     },
 
-
-
+    ]}
 
 });
