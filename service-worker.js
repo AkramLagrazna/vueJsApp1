89f006e6711ceb877e460429ce507f9e6d701930
Akram Lagrazna <lagrazna@outlook.it>
2021-03-01 06:04:59 -0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+var cacheName = 'afterclass-v1';
+var cacheFiles = ['webapps.html',
+                  'product.js',
+                  'afterclass.webmanifest',
+                  'images/icon-512.png'
+                 ];
